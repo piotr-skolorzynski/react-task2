62a16ab62ce35216f847c75ecc61a2320716f9a3
Piotr <p.skolorzynski@gmail.com>
2021-11-04 08:35:02 +0100

    create PlayerForm component move code responsible for rendering and handling form in Players component

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,26 +1,15 @@
 import { useState, useEffect } from "react";
 import Buttons from "./Buttons";
 import Table from "react-bootstrap/Table";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import PlayerRow from "./PlayerRow";
 import Statistics from "./Statistics";
 import "../assets/index.css";
-
-const generateRandomInteger = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) - min).toString();
-};
+import PlayerForm from "./PlayerForm";
 
 const Players = ({ playersToFetch }) => {
   const [highlightingColor, setHighlightingColor] = useState("#dc3545");
   const [players, setPlayers] = useState(null);
-  const [player, setPlayer] = useState({
-    id: null,
-    username: "",
-    points: null,
-  });
+
   const url = `http://localhost:8000/${playersToFetch}`;
 
   const changeColorToDanger = () => {
@@ -28,28 +17,6 @@ const Players = ({ playersToFetch }) => {
   };
   const changeColorToPrimary = () => {
     setHighlightingColor("#0d6efd");
-  };
-
-  const handleChange = (e) => {
-    setPlayer({
-      ...player,
-      username: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ...player,
-        id: generateRandomInteger(11, 100000),
-        points: generateRandomInteger(0, 200),
-      }),
-    }).catch((err) => console.log(err.message));
   };
 
   const removePlayer = (id) => {
@@ -74,30 +41,7 @@ const Players = ({ playersToFetch }) => {
         changeColorToDanger={changeColorToDanger}
         changeColorToPrimary={changeColorToPrimary}
       />
-      <Form className="form-addplayer" onSubmit={handleSubmit}>
-        <Form.Group
-          className="mb-3 mt-4 field-wrapper"
-          controlId="form-addplayer"
-        >
-          <Form.Label size="lg">Add new player</Form.Label>
-          <Form.Control
-            name="player"
-            value={player.username}
-            size="lg"
-            type="text"
-            placeholder="Enter player's name"
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Button
-          className="mb-4 input-button"
-          size="lg"
-          variant="primary"
-          type="submit"
-        >
-          Add
-        </Button>
-      </Form>
+      <PlayerForm url={url} />
       {players && (
         <Table borderless variant="dark" className="table-wrapper">
           <thead>
